e32feb4e30f5cae22c855ca36797548f8d7e724c
martinez1888 <78212114+martinez1888@users.noreply.github.com>
2022-09-11 23:48:20 -0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,19 +1,19 @@
 #BUCKET
-resource "aws_s3_bucket" "titiofodaopika" {
-  bucket = "titiofodaopika"
+resource "aws_s3_bucket" "titiofodaopika1" {
+  bucket = "titiofodaopika1"
 }
 
 #VERSIONAMENTO
-resource "aws_s3_bucket_versioning" "titiofodaopika" {
-  bucket = aws_s3_bucket.titiofodaopika.id
+resource "aws_s3_bucket_versioning" "titiofodaopika1" {
+  bucket = aws_s3_bucket.titiofodaopika1.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 #SITE ESTÁTICO
-resource "aws_s3_bucket_website_configuration" "titiofodaopika" {
-  bucket = aws_s3_bucket.titiofodaopika.id
+resource "aws_s3_bucket_website_configuration" "titiofodaopika1" {
+  bucket = aws_s3_bucket.titiofodaopika1.id
 
   index_document {
     suffix = "index.html"
@@ -25,8 +25,8 @@ resource "aws_s3_bucket_website_configuration" "titiofodaopika" {
 }
 
 #OBJECTS
-resource "aws_s3_bucket_object" "titiofodaopika" {
-    bucket   = aws_s3_bucket.titiofodaopika.id
+resource "aws_s3_bucket_object" "titiofodaopika1" {
+    bucket   = aws_s3_bucket.titiofodaopika1.id
     for_each = fileset("data/", "*")
     key      = each.value
     source   = "data/${each.value}"
@@ -34,8 +34,8 @@ resource "aws_s3_bucket_object" "titiofodaopika" {
 }
 
 #POLICY
-resource "aws_s3_bucket_policy" "titiofodaopika-policy" {
-  bucket = aws_s3_bucket.titiofodaopika.id
+resource "aws_s3_bucket_policy" "titiofodaopika-policy1" {
+  bucket = aws_s3_bucket.titiofodaopika1.id
 
   policy      = jsonencode({
     Version   = "2012-10-17"
@@ -44,7 +44,7 @@ resource "aws_s3_bucket_policy" "titiofodaopika-policy" {
         Effect    = "Allow",
         Principal = "*",
         Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::titiofodaopika/*",
+        Resource  = "arn:aws:s3:::titiofodaopika1/*",
       }
     ]
 	})
